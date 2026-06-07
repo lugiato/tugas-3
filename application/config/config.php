3cbc8adc13caae914ebcdeb7622bb6bd072af399
lugiato <asepbujang51@gmail.com>
2026-06-07 11:06:55 +0700

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pweb/';
+$config['base_url'] = 'http://localhost:8080/pweb-tugas-3/';
 
 /*
 |--------------------------------------------------------------------------
